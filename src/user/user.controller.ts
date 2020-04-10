b69cf422f7b7549fd4d99ab86cf3a5578f71aa48
Jose Claudio <jcsj.ti@gmail.com>
2020-04-10 13:55:49 -0300

    Aula 6

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService: UserService) {}
+
     @Post()
-    store(@Body() createUserDTO: CreateUserDTO) {
-        console.log(createUserDTO);
-        return 'OK';
+    async store(@Body() createUserDTO: CreateUserDTO) {
+        const user = new User();
+        Object.assign(user, createUserDTO);
+
+        return await this.userService.save(user);
     }
 }
